25eaad5fc3410d65bcb85429410a04934943aa84
PriyanshTri <Priyansh.tripathi_cs19@gla.ac.in>
2021-02-24 07:36:19 +0530

    temperary commit: every function is working fine but removeLast() is not working properly

--- src/main/MainClass.java
+++ src/main/MainClass.java
@@ -8,7 +8,28 @@ public class MainClass {
         MySinglyLinkedList lst=new MySinglyLinkedList();
         System.out.println(lst.isEmpty());    //true
         System.out.println(lst.size());         //0
-
-
+        lst.addFirst(10);
+        lst.addFirst(20);
+        lst.addFirst(30);
+        lst.addFirst(40);
+        lst.addFirst(50);
+        System.out.println(lst.size()); //5
+        lst.addLast(60);
+        lst.addAfterMiddle(35);
+        lst.traverse();
+        System.out.println(lst.addAfterGivenElement(36, 35));
+        System.out.println(lst.size()); //8
+        System.out.println(lst.first());
+        System.out.println(lst.last());
+        System.out.println(lst.lastWithoutTail());
+        System.out.println(lst.removeFirst()); //50
+        lst.traverse();
+        System.out.println(lst.removeLast());//60
+        lst.traverse();
+        System.out.println(lst.search(40)); //true
+        System.out.println(lst.search(45)); //false
+        lst.deleteAll();
+        System.out.println(lst.size()); //0
+        System.out.println(lst.isEmpty()); //true
     }
 }
